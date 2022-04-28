fe9d513d41f549646432da220a7b1dc7a2003c95
cruzpazv <36620068+cruzpazv@users.noreply.github.com>
2022-04-28 16:22:47 +0200

    Update installPackages.R

--- instructions/installPackages.R
+++ instructions/installPackages.R
@@ -1,29 +1,37 @@
-installifnot <- function(pckgName){
-  if (!(require(pckgName, character.only = TRUE))) {
-    install.packages(pckgName, dep = TRUE)
-    require(pckgName, character.only = TRUE)
-  }
-}
+############################################################
+###########                                         ########
+###########            Analysis miRNA               ########
+###########       Design by Victoria M. Cruz        ########
+###########             UEB - VHIR                  ########
+###########                2022                     ########
+###########                                         ########
+############################################################
 
-pk1 <- c("shiny", "shinydashboard", "DT", "reshape2", "ggplot2", "gplots", "scales", "plotly", "readxl", "glmnet", "ggvis", "shinyhelper",
-         "broom", "readr", "markdown", "ggthemes", "dplyr", "ggrepel", "ggfortify", "shinyBS", "glue", "tidyr", "devtools",
-         "Rcpp")
+# install CRAN packages
+pk_cran <- c("shiny", "shinydashboard", "DT", "reshape2", "ggplot2", "gplots", "scales", 
+         "plotly", "readxl", "glmnet", "ggvis", "shinyhelper", "broom", "readr", 
+         "markdown", "ggthemes", "dplyr", "ggrepel", "ggfortify", "shinyBS", "glue",
+         "tidyr", "devtools", "Rcpp", "dashboardthemes")
+install.packages(setdiff(pk_cran, rownames(installed.packages()))) 
 
-for (i in 1:length(pk1)){
-  installifnot(pk1[i])
-}
+# install BiocManager in order of install bioconductor pckgs, if needed
+if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 
-installBiocifnot <- function(pckgName){
-  if (!(require(pckgName, character.only = TRUE))) {
-    if (!require("BiocManager", quietly = TRUE))
-      install.packages("BiocManager")
-    BiocManager::install(pckgName)
-    require(pckgName, character.only = TRUE)
-  }
-}
+# install Bioconductor packages
+pk_bioc <- c("impute")
+BiocManager::install(setdiff(pk_bioc, rownames(installed.packages())), update = TRUE, ask = FALSE)
 
-installBiocifnot("impute")
+# install Github packages
+pk_git <- c("vqv/ggbiplot")
+devtools::install_github(setdiff(pk_git, rownames(installed.packages())))
 
-devtools::install_github("vqv/ggbiplot")
-devtools::install_github("nik01010/dashboardthemes")
 
+# needed for miRFA
+pk_cran_miRFA <- c("argparser", "DBI", "RSQLite","VennDiagram","gridExtra") # CRAN
+install.packages(setdiff(pk_cran_miRFA, rownames(installed.packages())))
+
+pk_bioc_miRFA <- c("org.Hs.eg.db", "GO.db", "edgeR") # Bioconductor
+BiocManager::install(setdiff(pk_bioc_miRFA, rownames(installed.packages())), version = "3.8", update = TRUE, ask = FALSE)
+
+# REQUIRE ALL PACKAGES
+require(c(pk_cran, pk_bioc, pk_git, pk_cran_miRFA, pk_bioc_miRFA), character.only = TRUE)
